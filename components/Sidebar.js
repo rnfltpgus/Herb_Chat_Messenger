6afa9892df293cadc31dab73ba7519615ceff6ad
rnfltpgus <jpp901220@gmail.com>
2022-08-12 00:54:08 +0900

    feat, fix : sidebar, index 영역 구분 생성, className not match(스타일컴포넌트 오류 해결)

---
+++ components/Sidebar.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Sidebar = () => {
+  return <Container>Sidebar 영역</Container>;
+};
+
+export default Sidebar;
+const Container = styled.div`
+  background-color: #88d9ff;
+  min-width: 320px;
+  max-width: 450px;
+  height: 100%;
+`;
